2ec56748899f29e304963973f0728ed411ab7e6c
Yara-Darwish <yara25001690@gmail.com>
2026-09-09 02:01:51 +0300

    fixing a typo

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import {GenerateRandomNumer} from './utils.js';
+import {GenerateRandomNumber} from './utils.js';
 import fs from 'fs/promises';
 import url from 'url';
 import path from 'path';
