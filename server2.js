43f3df95c14291fa5e7dadca1558b9fae1418e6b
Yara-Darwish <yara25001690@gmail.com>
2026-09-11 22:15:01 +0300

    finding the id

--- server2.js
+++ server2.js
@@ -11,6 +11,16 @@ const users = [
             res.write(JSON.stringify(users));
             res.end();
         }
+        // [0-9]+: يقبل رقم من خانة واحدة أو أكتر (1, 23, 100)
+        // [0-9]: يقبل رقم من خانة واحدة بس (من 0 لـ 9)
+        // $: بيتأكد إن السطر خلص عند الرقم ده وما تزدش عليه أجزاء تانية في الـ URL
+        else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
+            const id =req.url.split('/')[3];
+            console.log(id);
+                res.setHeader('Content-Type','application/json');
+                res.write(JSON.stringify({id: 1,name: "yara"}));
+                res.end();
+        }
         else{
             res.setHeader('Content-Type','application/json');
             res.statusCode= 404;
