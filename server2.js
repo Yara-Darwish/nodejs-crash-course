9981aeb66bfe29799805a32c1fe733521ef87cfe
Yara-Darwish <yara25001690@gmail.com>
2026-09-13 00:42:34 +0300

    removing the extra lines of code becuase that was too many res.end() it was also useless

--- server2.js
+++ server2.js
@@ -11,15 +11,21 @@ const users = [
             res.write(JSON.stringify(users));
             res.end();
         }
-        // [0-9]+: يقبل رقم من خانة واحدة أو أكتر (1, 23, 100)
-        // [0-9]: يقبل رقم من خانة واحدة بس (من 0 لـ 9)
-        // $: بيتأكد إن السطر خلص عند الرقم ده وما تزدش عليه أجزاء تانية في الـ URL
         else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
-            const id =req.url.split('/')[3];
-            console.log(id);
+            const id = req.url.split('/')[3];
+            const user = users.find((user) => user.id === parseInt(id));
+            if(user){
+                console.log(id);
+                    res.setHeader('Content-Type','application/json');
+                    res.write(JSON.stringify(user));
+                    res.end();
+            }
+            else{
                 res.setHeader('Content-Type','application/json');
-                res.write(JSON.stringify({id: 1,name: "yara"}));
+                res.statusCode= 404;
+                res.write(JSON.stringify({message: 'User not found'}));
                 res.end();
+            }
         }
         else{
             res.setHeader('Content-Type','application/json');
